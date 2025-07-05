9fe8008b1ed99b97db0f8cae31b9c24b1c8b436b
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-07-05 12:42:49 -0300

    Delete src/app/quantec-terapia-quantica directory

--- src/app/quantec-terapia-quantica/page.tsx
+++
@@ -1,1 +0,0 @@
-import LandingPage from '@/components/LandingPage'; import { LandingPageData } from '@/types/lp-config'; import lpData from './lp.json';  const config = lpData as LandingPageData;  export default function QuantecPage() {   return <LandingPage data={config} />; }
